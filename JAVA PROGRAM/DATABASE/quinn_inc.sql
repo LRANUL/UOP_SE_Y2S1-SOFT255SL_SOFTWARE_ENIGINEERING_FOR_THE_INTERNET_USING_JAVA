1591a25facd74ba9574fc70029775e95a0ab2d96
Ranul <48907530+LRANUL@users.noreply.github.com>
2019-10-27 09:38:31 +0530

    Phase 1.1

--- JAVA PROGRAM/DATABASE/quinn_inc.sql
+++ JAVA PROGRAM/DATABASE/quinn_inc.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Oct 26, 2019 at 03:31 PM
+-- Generation Time: Oct 27, 2019 at 04:07 AM
 -- Server version: 5.7.26
 -- PHP Version: 7.2.18
 
@@ -33,7 +33,24 @@ CREATE TABLE IF NOT EXISTS `login` (
   `UserName` varchar(100) NOT NULL,
   `Password` varchar(255) NOT NULL,
   `Hash` varchar(255) NOT NULL,
-  `Email` varchar(255) NOT NULL
+  `Email` varchar(255) NOT NULL,
+  PRIMARY KEY (`UserName`,`Email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transactions`
+--
+
+DROP TABLE IF EXISTS `transactions`;
+CREATE TABLE IF NOT EXISTS `transactions` (
+  `Type` char(1) NOT NULL,
+  `Account_Number` bigint(40) NOT NULL,
+  `Account_Type` varchar(200) NOT NULL,
+  `Amount` float NOT NULL DEFAULT '0',
+  `Date_Time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`Account_Number`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 COMMIT;
 
